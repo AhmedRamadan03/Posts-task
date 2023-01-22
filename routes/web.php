4a96c67c7b0d165091d6694e84fa512ecb2ef78f
AhmedRamadan09 <105635818+AhmedRamadan09@users.noreply.github.com>
2023-01-22 03:31:33 +0200

    init-comment

--- routes/web.php
+++ routes/web.php
@@ -15,27 +15,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect()->route('login');
-});
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-route::group(['middleware' => 'auth:web'], function () {
-
-    // Route::resource('posts' , PostController::class);
-    Route::post('posts/store' , [PostController::class , 'store'])->name('posts.store');
-    Route::post('posts/update/{post}' , [PostController::class , 'update'])->name('posts.update');
-    Route::get('posts/destroy/{post}' , [PostController::class , 'destroy'])->name('posts.destroy');
-
-
-    Route::post('comments/store' , [CommentController::class , 'store'])->name('comments.store');
-    Route::get('comments/destroy/{comment}' , [CommentController::class , 'destroy'])->name('comments.destroy');
-});
-
-
-Route::any('{url}', function(){
-    return view('404');
-})->where('url', '.*');
